3acc9c88393cd6df123a2793def6e3a74e9ee411
zzc <xabodog1981@gmail.com>
2016-07-14 21:03:02 +0800

    Initial Commit

---
+++ text/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  text
+//
+//  Created by qingyun on 16/7/14.
+//  Copyright © 2016年 zzc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
